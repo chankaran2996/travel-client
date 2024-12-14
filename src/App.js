5ee276eeab7f4b2f78c7aa2282f8ec5e309d6800
chankaran2996 <chandrasekaran2996@gmail.com>
2024-12-14 22:44:36 +0530

    1.1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import Footer from './Components/Footer/Footer';
+import Home from './Components/Home/Home';
+import Main from './Components/Main/Main';
+import Navbar from './Components/Navbar/Navbar';
 
 const App = () =>  {
   return (
-    <div >
-<h1>welcome</h1>
-    </div>
+   <>
+   <Navbar/>
+   <Main/>
+   <Home/>
+   <Footer/>
+   </>
   );
 }
 
