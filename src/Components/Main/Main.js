ecd7d1ca9b209223e714bd39f2bc34ab2f001f0e
chankaran2996 <chandrasekaran2996@gmail.com>
2024-12-20 20:55:44 +0530

    8.0

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,8 +1,94 @@
 import React from 'react'
 import './main.css'
+
+const data = [
+  {
+    id:1,
+    imgUrl:"img",
+    destTitle: "Bora Bora",
+    loaction: "New Zealand",
+    grade:"CULTURAL RELAX",
+    fees:"₹700",
+    description: ""
+  },
+  {
+    id:2,
+    imgUrl:"img",
+    destTitle: "Bora Bora",
+    loaction: "New Zealand",
+    grade:"CULTURAL RELAX",
+    fees:"₹700",
+    description: ""
+  },
+  {
+    id:3,
+    imgUrl:"img",
+    destTitle: "Bora Bora",
+    loaction: "New Zealand",
+    grade:"CULTURAL RELAX",
+    fees:"₹700",
+    description: ""
+  },
+  {
+    id:4,
+    imgUrl:"img",
+    destTitle: "Bora Bora",
+    loaction: "New Zealand",
+    grade:"CULTURAL RELAX",
+    fees:"₹700",
+    description: ""
+  },
+  {
+    id:5,
+    imgUrl:"img",
+    destTitle: "Bora Bora",
+    loaction: "New Zealand",
+    grade:"CULTURAL RELAX",
+    fees:"₹700",
+    description: ""
+  },
+  {
+    id:6,
+    imgUrl:"img",
+    destTitle: "Bora Bora",
+    loaction: "New Zealand",
+    grade:"CULTURAL RELAX",
+    fees:"₹700",
+    description: ""
+  },
+  {
+    id:7,
+    imgUrl:"img",
+    destTitle: "Bora Bora",
+    loaction: "New Zealand",
+    grade:"CULTURAL RELAX",
+    fees:"₹700",
+    description: ""
+  },
+  {
+    id:8,
+    imgUrl:"img",
+    destTitle: "Bora Bora",
+    loaction: "New Zealand",
+    grade:"CULTURAL RELAX",
+    fees:"₹700",
+    description: ""
+  },
+  {
+    id:9,
+    imgUrl:"img",
+    destTitle: "Bora Bora",
+    loaction: "New Zealand",
+    grade:"CULTURAL RELAX",
+    fees:"₹700",
+    description: ""
+  }
+]
+
 const Main = () => {
   return (
     <section className='main container section'>
+
       <div className='secTitle'>
         <h3 className='title'>
           Most visited destination
@@ -10,7 +96,7 @@ const Main = () => {
       </div>
 
       <div className='secContent grid'>
-        
+
       </div>
     </section>
   )
