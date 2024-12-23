e2ea25b93a01542f974bbed3c162ae10416a1248
chankaran2996 <chandrasekaran2996@gmail.com>
2024-12-23 22:53:27 +0530

    11.0

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import './footer.css'
+import videoF from '../../Assets/viedoF.mp4'
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <section className='footer'>
+      <div className='viedoDiv'>
+        <video src={videoF} loop autoPlay muted type="video/mp4"></video>
+      </div>
+    </section>
   )
 }
 
